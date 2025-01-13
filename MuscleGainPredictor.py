42c20da03d3a7989d2a28f0141431ef3b4a41c2d
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-01-13 15:32:13 +0100

    adding new stuff gathered all the x features i want to work with

--- MuscleGainPredictor.py
+++ MuscleGainPredictor.py
@@ -35,19 +35,51 @@ def get_training_time(data):
         
     return None
 
+# function to get the weight(kg) cause that could also be a factor not sure yet
+def get_weight(data):
+    for column in data.columns:
+        if '(kg)' in column.lower().strip():
+            return column
+    
+    return None
 
 
+# function to get energy intake kcal kg day that should be a major factor aswell 
+def get_ernergy_intake(data):
+
+    for column in data.columns:
+        if "Energy" and "intake"in column.lower().strip():
+            return column
+        
+    return None
+
+# function to get the duration (weeks) thats also a major factor then later i will filter out to have the same weeks 
+def get_weeks(data):
+
+    for column in data.columns:
+        if "Duration " and "(weeks)" in column.lower().strip():
+            return column
+        
+    return None
 
 
 # Get the relevant column names
 protein = get_protein_intake(data)
 LBM_change = get_LBM_change(data)
 trainingTime = get_training_time(data)
+weight= get_weight(data)
 
+energy = get_ernergy_intake(data)
+weeks = get_weeks(data)
 # Print the column names for debugging
 print(f"Protein intake column found: {protein}")
 print(f"LBM change column found: {LBM_change}")
 print(f"training time column found: {trainingTime}")
+print(f"weight(kg) column found: {weight}")
+
+print(f"energy column found: {energy}")
+print(f"weeks column found: {weeks}")
+
 
 # Check if the columns were found
 if protein and LBM_change:
@@ -76,3 +108,56 @@ if trainingTime and LBM_change:
     plt.title(f"{trainingTime} and the resulting {LBM_change}")
     plt.legend()
     plt.show()
+
+
+if weight and LBM_change:
+    x_data = data[weight]
+    y_data = data[LBM_change]
+
+    #plot data
+    plt.scatter(x_data, y_data, c="green", label="data points")
+    plt.xlabel(weight)
+    plt.ylabel(LBM_change)
+    plt.title(f"relationship between {weight} and {LBM_change}")
+    plt.legend()
+    plt.show()
+
+
+
+if energy and LBM_change:
+
+    x_data = data[energy]
+    y_data = data[LBM_change]
+
+    #plot data
+    plt.scatter(x_data, y_data, c="yellow", label="data points")
+    plt.xlabel(energy)
+    plt.ylabel(LBM_change)
+    plt.title(f"relationship between {energy} and {LBM_change}")
+    plt.legend()
+    plt.show()
+
+
+if weeks and LBM_change:
+
+    x_data = data[weeks]
+    y_data = data[LBM_change]
+
+    #plot data
+    plt.scatter(x_data, y_data, c="red" , label="data poiint")
+    plt.xlabel(weeks)
+    plt.ylabel(LBM_change)
+    plt.title(f"relationship between {weeks} and {LBM_change}")
+    plt.legend()
+    plt.show()
+
+# picking good set of x feature to find a balance for the regression model
+
+x_train = [protein, energy, trainingTime]
+y_train = [LBM_change]
+
+
+
+def compute_cost(X,y,w,b):
+    m, = X.shape()
+    
